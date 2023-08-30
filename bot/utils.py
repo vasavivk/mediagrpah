b52dcfd70ac4243ed974df2761e6f0997651b294
Vasavi <85066181+vasavivk@users.noreply.github.com>
2023-08-30 13:51:39 +0530

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -2,10 +2,10 @@ from telegraph import Telegraph
 import re
 import subprocess
 
-def create_telegraph_page(html_text):
+def create_telegraph_page(html_text, title):
     telegraph = Telegraph(access_token="df37be0d94ded1eff095b0c9d5e43268dcf039f597e110433945b83f5aac", domain="graph.org")
     
-    response = telegraph.create_page('MediaInfo',author_name='MediaInfoBot', author_url="https://telegram.dog/applemuiscdlBot, html_content=html_text)
+    response = telegraph.create_page(f'MediaInfo of {title}',author_name='MediaInfoBot', author_url="https://telegram.dog/applemuiscdlBot, html_content=html_text)
     return response["url"]
 
 def format_sections(input_text):
@@ -23,9 +23,9 @@ def format_sections(input_text):
     return formatted_output 
 
 
-def manger(raw_info,title):
+def manger(raw_info, title):
     format_info= format_sections(raw_info)
-    pre_link= create_telegraph_page(format_info)
+    pre_link= create_telegraph_page(format_info, title)
     return pre_link
     
 
